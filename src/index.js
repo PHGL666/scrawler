e8f16cc7a98830fad901a9c688aa87f7c0df646a
Gotzsche <pierregotzsche@gmail.com>
2019-07-25 22:52:52 +0200

    push commit final le vrai

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {createStore, applyMiddleware, compose} from "redux";
 import {Provider} from "react-redux";
-import todoApplication from "./reducers/reducers";
+import scrawlyApplication from "./reducers/reducers";
 import { BrowserRouter } from "react-router-dom";
 import thunk from 'redux-thunk';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(todoApplication, composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(scrawlyApplication, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(<Provider store={store}><BrowserRouter><App/></BrowserRouter></Provider>, document.getElementById('root'));
 
